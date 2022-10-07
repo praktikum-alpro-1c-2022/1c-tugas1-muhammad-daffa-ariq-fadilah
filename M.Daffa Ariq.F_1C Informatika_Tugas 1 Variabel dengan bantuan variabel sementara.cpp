00b32133540ad24a6eac644cb0e427ed34a7640b
Muhammad Daffa Ariq Fadilah <114640665+Muhammad-Daffa-Ariq-Fadilah@users.noreply.github.com>
2022-10-07 20:30:11 +0700

    Update M.Daffa Ariq.F_1C Informatika_Tugas 1 Variabel dengan bantuan variabel sementara.cpp

--- M.Daffa Ariq.F_1C Informatika_Tugas 1 Variabel dengan bantuan variabel sementara.cpp
+++ M.Daffa Ariq.F_1C Informatika_Tugas 1 Variabel dengan bantuan variabel sementara.cpp
@@ -9,9 +9,9 @@ int main(){
     x = 69; //Variabel awal
     y = 420; //Variabel awal
 
-    z = x; // Variabel z ditukar dengan Variabel x yang bernilai 5
-    x = y; // Variabel x ditukar dengan Variabel y yang bernilai 10
-    y = z; // Variabel y ditukar dengan Variabel z yang bernilai 5
+    z = x; // Variabel z ditukar dengan Variabel x yang bernilai 69
+    x = y; // Variabel x ditukar dengan Variabel y yang bernilai 420
+    y = z; // Variabel y ditukar dengan Variabel z yang bernilai 69
 
     cout << "Menukarkan 2 Variabel dengan bantuan Variabel Sementara\n";
     cout << "x : " << x << endl << "Y : " << y << endl;
